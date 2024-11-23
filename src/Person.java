ee14376695a7e148c76cf694b6f07c4c970df67b
JishenWang <1801076890@qq.com>
2024-11-23 10:29:33 +0800

    1
    Five classes added: Person class added four instance variables: Name, age, gender, phone number and Add a total default constructor and a total parameter constructor, add getters and setters for all Person instance variables, add three instance variables for Employee class: position, salary, employeeID and other same as the Person class, the Visitor class adds three instance variables: ticketType, hasFast Pass, visitDate, Ride class: Three instance variables were added: rideName, rideType, rideOperator, and AssignmentTwo class.

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,55 @@
 public class Person {
+    private String name;
+    private int age;
+    private String gender;
+    private String phoneNumber; 
 
+    // Added a default constructor
+    public Person() {
+        this.name = "";
+        this.age = 0;
+        this.gender = "";
+        this.phoneNumber = ""; 
+    }
+
+    // Constructor with parameters
+    public Person(String name, int age, String gender, String phoneNumber) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber; 
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
--- src/Ride.java
+++ src/Ride.java
@@ -1,3 +1,45 @@
 public class Ride {
+    private String rideName;
+    private String rideType;
+    private Employee rideOperator;
 
+    // Added a default constructor
+    public Ride() {
+        this.rideName = "";
+        this.rideType = "";
+        this.rideOperator = null;
+    }
+
+    // Constructor with parameters
+    public Ride(String rideName, String rideType, Employee rideOperator) {
+        this.rideName = rideName;
+        this.rideType = rideType;
+        this.rideOperator = rideOperator;
+    }
+
+    // Getters and Setters
+    public String getRideName() {
+        return rideName;
+    }
+
+    public void setRideName(String rideName) {
+        this.rideName = rideName;
+    }
+
+    public String getRideType() {
+        return rideType;
+    }
+
+    public void setRideType(String rideType) {
+        this.rideType = rideType;
+    }
+
+    public Employee getRideOperator() {
+        return rideOperator;
+    }
+
+    public void setRideOperator(Employee rideOperator) {
+        this.rideOperator = rideOperator;
+    }
 }
+
